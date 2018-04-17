e1ed82f42076609b997cc9c65ad8fa2e81d082a2
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2018-04-17 16:12:00 +0200

    Update select lang component

--- src/components/SelectLanguage.js
+++ src/components/SelectLanguage.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 import { FormattedMessage, injectIntl, intlShape, FormattedRelative } from 'react-intl'
 
 const SelectLanguage = (props) => {
-  console.log('Select lang', props)
+  //console.log('Select lang', props)
   const links = props.langs.map(lang =>
     <Link to={lang.link} key={lang.langKey} className={`navbar-item ${(props.intl.locale === lang.langKey ? 'is-active' : '')}`}>
       <span className={`flag-icon flag-icon-${props.languages.find(x => x.key === lang.langKey).flagIconCode}`}></span>
@@ -18,7 +18,7 @@ const SelectLanguage = (props) => {
         <span className={`flag-icon flag-icon-${props.languages.find(x => x.key === props.intl.locale).flagIconCode}`}></span>
         <FormattedMessage id={props.intl.locale} defaultMessage='CurrentLangName' description='CurrentLangName' />
       </a>
-      <div className="navbar-dropdown is-boxed0 is-inverted0 is-outlined0">
+      <div className="navbar-dropdown">
         {links}
       </div>
     </div>
