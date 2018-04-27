d8a949c15da2ad16ed98df2c7c4bdf0de2b1f60e
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2018-04-27 17:03:54 +0200

    Refactoring products into cms - part 2/2 - fix cms config

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -2,11 +2,7 @@ import React from 'react'
 import { ProductPageTemplate } from '../../templates/product-page'
 
 const ProductPagePreview = ({ entry, getAsset }) => {
-  console.log('cms - entry', entry)
-  console.log('cms - title', entry.getIn(['data', 'title']))
-  console.log('cms - title', entry.getIn(['data', 'meta']))
-  console.log('cms - products', entry.getIn(['data', 'products']).toJS())
-  console.log('cms - getAsset', getAsset)
+
   const entryProducts= entry.getIn(['data', 'products'])
   const products = entryProducts ? entryProducts.toJS() : []
 
