91f0edca2c1e22df6d2832de9d15fdcda2b68b3b
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2018-05-09 14:40:16 +0200

    Add Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 const languages = require('./src/data/languages')
-
 //console.log('languages>', languages)
+const gtm = `GTM-TJZWRGC`
 
 module.exports = {
   siteMetadata: {
     title: 'Czech Milk',
     languages,
-    ga: `GA-123456`,
+    gtm: `${gtm}`,
   },
   plugins: [
     {
@@ -91,5 +91,19 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+        resolve: `gatsby-plugin-google-tagmanager`,
+        options: {
+          id: `${gtm}`,
+
+          // Include GTM in development.
+          // Defaults to false meaning GTM will only be loaded in production.
+          includeInDevelopment: false,
+
+          // Specify optional GTM environment details.
+          //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+          //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+        },
+      },
   ],
 }
