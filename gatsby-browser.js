8fbb562267d5e06c625a449dbd5fd16fe0ca8a25
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2018-05-23 17:58:35 +0200

    Try fix build error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import objectFitImages from 'object-fit-images'
 
-exports.registerServiceWorker = () => true
+//exports.registerServiceWorker = () => true
 
 exports.onInitialClientRender = () => {
   //console.log("ReactDOM.render has executed")
@@ -16,28 +16,31 @@ exports.onInitialClientRender = () => {
 
 exports.onRouteUpdate = args => {
   //console.log('onRouteUpdate', args)
-  if (typeof window !== 'undefined' && args.action == "PUSH") {
+  //if (typeof window !== 'undefined' && args.action == "PUSH") {
     //console.log('scroll TOP...')
-  }
+  //}
 }
 
 exports.shouldUpdateScroll = args => {
   if (typeof window !== 'undefined') {
+    /*
     if(location.pathname.indexOf("/thank-you") !== -1) {
       setTimeout(() => {
         const anchor = document.querySelector('#thank-you')
         window.scrollInstance.animateScroll(anchor)
       }, 100)
     }
+    */
   }
 
   // Force updae scroll
   return true
+
   //  scroll on < back, forward >, refresh, but not link clicks
-  const shouldUpdateScroll = (
-    args.prevRouterProps == null ||
-    (args.prevRouterProps && args.prevRouterProps.history.action == "POP")
-  ) ? true : false
-  console.log('shouldUpdateScroll', shouldUpdateScroll, args)
-  return shouldUpdateScroll
+  // const shouldUpdateScroll = (
+  //   args.prevRouterProps == null ||
+  //   (args.prevRouterProps && args.prevRouterProps.history.action == "POP")
+  // ) ? true : false
+  // console.log('shouldUpdateScroll', shouldUpdateScroll, args)
+  // return shouldUpdateScroll
 }
